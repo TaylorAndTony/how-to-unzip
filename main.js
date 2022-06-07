98dd61df3493dd0e6197fb95181822132a6b9cb4
TaylorAndTony <doyijian@163.com>
2022-06-07 20:30:42 +0800

    use class to modify bottom line

---
+++ main.js
@@ -0,0 +1,65 @@
+class RandomSaying {
+    constructor() {
+        this.sayings = [
+            "If not love, do not harm.",
+            "The way you've written your algorithm is changing the way we lead out country.",
+            "It's not magic, it's the power of abstraction!",
+            "你不抬头看星星，如何导航?",
+            "百年前物理学大厦上的两朵乌云，成了咱们驶向星辰大海的船桨",
+            "我用新奇的目光盯着地上的积雪，不，那是质子在地球的磁场中进动<br>——核磁共振成像诺奖得主保罗·劳特伯",
+            "它在等待，等待量子力学，凝聚态物理，磁场工程，等待某个微小的进步，扰动整个未来。",
+            "纸上得来终觉浅，自己实验最保险<br>——毕导THU"
+        ];
+        this.length = this.sayings.length;
+        this.nowAt = 0;
+        this.clickCount = 0;
+        this.rand();
+    };
+    /**
+     * Mount this to DOM, when click, random give a saying
+     * @param {string} selector Where to mount
+     */
+    mount(selector) {
+        console.log(this.nowAt);
+        let obj = document.querySelector(selector);
+        obj.addEventListener('click', () => {
+            this.handleClickWitch(obj);
+        });
+        return this;
+    };
+    /**
+     * Handle click event
+     * @param {object} obj Which obj is clicked.
+     */
+    handleClickWitch(obj) {
+        // log click count
+        this.clickCount++;
+        if (this.clickCount > 30) {
+            obj.innerHTML = '<strong>我被玩坏啦！</strong>';
+            return;
+        }
+        // one roll is done
+        if (this.nowAt >= this.length) {
+            this.nowAt = 0;
+            this.rand();
+        }
+        // update saying
+        obj.innerHTML = this.sayings[this.nowAt];
+        this.nowAt++;
+    };
+    /**
+     * Shuffile the sayings
+     */
+    rand() {
+        this.sayings.sort(function () {
+            return Math.random() - 0.5
+        });
+        return this;
+    };
+}
+
+
+
+window.onload = function () {
+    new RandomSaying().mount('#witch');;
+}
